1d77a66dd19e8ca0f555aa110e515aac113aa70b
MinsuKim <minsu020302@gmail.com>
2022-02-11 19:48:42 +0900

    add: @post

--- nestjs-board-app/src/boards/boards.controller.ts
+++ nestjs-board-app/src/boards/boards.controller.ts
@@ -1,10 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { appendFile } from 'fs';
+import { Settings } from 'http2';
+import { Board } from './board.model';
 import { BoardsService } from './boards.service';
 
 @Controller('boards')
 export class BoardsController {
+    constructor(private boardsService: BoardsService) { }
+    
+    @Get('/')
+    getAllBoard(): Board[] {
+        return this.boardsService.getAllBoards();
+    }
 
-    constructor(private boardsService: BoardsService){
-
+    @Post()
+    creatBoard(
+        @Body('title') title: string,
+        @Body('description') description: string
+    ): Board {
+        return this.boardsService.creatBoard(title,description);
     }
 }
--- nestjs-board-app/src/boards/boards.service.ts
+++ nestjs-board-app/src/boards/boards.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from '@nestjs/common';
-
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
 @Injectable()
 export class BoardsService {
-    private boards = [];
+    private boards: Board[] = [];
 
-    getAllBoards(){
+    getAllBoards(): Board[] {
         return this.boards;
+    }
+    creatBoard(title: string, description: string){
+        const board: Board = {
+            id: uuid(),
+            title: title,
+            description, //자바스크립트에서 이름과 인수랑같다면 생략가능 14줄 title: title과 같음
+            status: BoardStatus.PUBLIC,
+        }
+        
+        this.boards.push(board);
+        return board;
     }
 }
