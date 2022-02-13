3821f1c3b828699bface54225d481194348f94f2
MinsuKim <minsu020302@gmail.com>
2022-02-13 20:06:42 +0900

    add: create, delete, update methods

--- nestjs-board-app/src/boards/boards.controller.ts
+++ nestjs-board-app/src/boards/boards.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { appendFile } from 'fs';
 import { Settings } from 'http2';
-import { Board } from './board.model';
+import { CreatBoardDto} from './dto/create-board.dto';
+import { Board, BoardStatus } from './board.model';
 import { BoardsService } from './boards.service';
 
 @Controller('boards')
@@ -13,11 +14,28 @@ export class BoardsController {
         return this.boardsService.getAllBoards();
     }
 
+    @Get('/:id')
+    getBoardById(@Param('id') id: string): Board {
+        return this.boardsService.getBoardById(id)
+    } 
+
     @Post()
     creatBoard(
-        @Body('title') title: string,
-        @Body('description') description: string
+        @Body() createBoardDto: CreatBoardDto
     ): Board {
-        return this.boardsService.creatBoard(title,description);
+        return this.boardsService.creatBoard(createBoardDto);
+    }
+
+    @Delete('/:id')
+    delteBoard(@Param('id') id: string):void {
+        this.boardsService.deleteBoard(id);
+    }
+
+    @Patch('/:id/status')
+    updateBoardStatus(
+        @Param('id') id: string,
+        @Body('status') status: BoardStatus
+    ) {
+        return this.boardsService.updateBoardStatus(id, status)
     }
 }
